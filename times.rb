f81900a83eb0ce93429bc15ba56f80ee11d75607
Sara Isabel Garcia Reyes <sarisgar28@gmail.com>
2020-03-05 22:21:22 +0000

    Done.

--- times.rb
+++ times.rb
@@ -1,9 +1,5 @@
-def using_times
+def using_times 
+  7.times do 
   puts "Wingardium Leviosa"
-  puts "Wingardium Leviosa"
-  puts "Wingardium Leviosa"
-  puts "Wingardium Leviosa"
-  puts "Wingardium Leviosa"
-  puts "Wingardium Leviosa"
-  puts "Wingardium Leviosa"
+end 
 end 
