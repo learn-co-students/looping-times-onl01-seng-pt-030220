61bf9aa785571d036cd477ddc3eb6183bbf33f88
Pablo Menz <p.menz.r@gmail.com>
2020-03-07 18:46:59 +0000

    Done.

--- times.rb
+++ times.rb
@@ -1,7 +1,14 @@
 def using_times
-	#your code here
+	7.times do 
+	  puts "Wingardium Leviosa"
+end
 end
 
 
 
 
+#We're still struggling to master that levitation charm.
+#Since we need to keep practicing, let's write some code 
+#that allows us to `puts` the phrase "Wingardium Leviosa"
+#only seven times (instead of infinitely, like when we 
+#used just a plain `loop` construct).
